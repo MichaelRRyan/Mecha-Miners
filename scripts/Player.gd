7a606a4c2f24949e1987c2374925d6ff17e61c14
MichaelRRyan <c00239510@itcarlow.ie>
2021-11-02 16:48:23 +0000

    Added simple comments and spacing

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,16 +6,22 @@ export var gravity_acceleration = 256.0
 
 var velocity = Vector2.ZERO
 
+
 func _process(delta):
 	
+	# Add the gravity acceleration to velocity.
 	velocity.y += gravity_acceleration * delta
 	
+	# Add jump speed to velocity on jump input if on the ground.
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = jump_speed
 	
+	# Get the horizontal input.
 	var hor_input = (Input.get_action_strength("move_right") - 
 					 Input.get_action_strength("move_left"))
 	
+	# Set the horizontal velocity as the horizontal input times movement speed.
 	velocity.x = hor_input * movement_speed
 	
+	# Move by the velocity.
 	var _result = move_and_slide(velocity, Vector2.UP)
