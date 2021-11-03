6b0d3e8b97552e40b550a4b88aac3cc0f8963b01
MichaelRRyan <c00239510@itcarlow.ie>
2021-11-03 15:12:38 +0000

    Completed Advanced Horizontal Movement

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,14 +1,60 @@
 extends KinematicBody2D
 
-export var movement_speed = 64.0
+tool
+
 export var jump_speed = -160.0 # Negative so it's upwards.
 export var gravity_acceleration = 256.0
 
+var max_speed = 50.0 # Pixels / Second
+var time_to_max_speed = 0.1 # Seconds
+var time_to_full_stop = 0.1 # Seconds
+
+var acceleration = 0.0
+var deceleration = 0.0
 var velocity = Vector2.ZERO
 
 
+# -- Param Map --
+var params = {
+	"horizontal_movement/max_speed": max_speed,
+	"horizontal_movement/time_to_max_speed": time_to_max_speed,
+	"horizontal_movement/time_to_full_stop": time_to_full_stop,
+}
+
+func _set(property, value):
+	if params.has(property):
+		params[property] = value
+		
+		if property == "horizontal_movement/time_to_max_speed":
+			acceleration = max_speed / value
+		
+		elif property == "horizontal_movement/time_to_full_stop":
+			deceleration = max_speed / value
+		
+		return true
+
+
+func _get(property):
+	if params.has(property):
+		return params[property]
+
+
+func _get_property_list():
+	return [
+		{ name = "horizontal_movement/max_speed", type = TYPE_REAL },
+		{ name = "horizontal_movement/time_to_max_speed", type = TYPE_REAL },
+		{ name = "horizontal_movement/time_to_full_stop", type = TYPE_REAL },
+	]
+
+
+func _ready():
+	print(str(acceleration) + " " + str(deceleration))
+
+
 func _physics_process(delta):
-	
+	if Engine.editor_hint:
+		return
+		
 	# Add the gravity acceleration to velocity.
 	velocity.y += gravity_acceleration * delta
 	
@@ -17,11 +63,20 @@ func _physics_process(delta):
 		velocity.y = jump_speed
 	
 	# Get the horizontal input.
-	var hor_input = (Input.get_action_strength("move_right") - 
+	var direction = (Input.get_action_strength("move_right") - 
 					 Input.get_action_strength("move_left"))
 	
-	# Set the horizontal velocity as the horizontal input times movement speed.
-	velocity.x = hor_input * movement_speed
+	# If there's no input.
+	if direction == 0.0:
+		velocity.x -= min(deceleration * delta, abs(velocity.x)) * sign(velocity.x)
+	
+	# If there is input.
+	else:
+		velocity.x  += acceleration * direction * delta
+	
+	# Clamps the horizontal movement to the max speed.
+	if abs(velocity.x) > max_speed:
+		velocity.x = max_speed * sign(velocity.x)
 	
 	# Move by the velocity.
 	velocity = move_and_slide(velocity, Vector2.UP)
