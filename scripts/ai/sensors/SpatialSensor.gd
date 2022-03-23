ff166d4944ad921b2894a172a35615f2737cdce3
Michael R Ryan <c00239510@itcarlow.ie>
2022-03-23 12:24:17 +0000

    Further refactored spatial sensor

--- scripts/ai/sensors/SpatialSensor.gd
+++ scripts/ai/sensors/SpatialSensor.gd
@@ -2,22 +2,23 @@ extends Node2D
 
 signal new_furthest(furthest_cell)
 
-
+# Dependancies.
 var _terrain : Terrain = null
+ 
+# Public variables.
+var view_range_cells : int = 8 setget set_view_range_cells
 
+# Constants.
 const CELL_SIZE : float = 16.0
 const RECALCULATE_DISTANCE : float = CELL_SIZE * 3.0
 const RECALCULATE_DISTANCE_SQUARED : float = RECALCULATE_DISTANCE * RECALCULATE_DISTANCE
- 
-var view_range_cells_squared : int = 8 * 8
 
+# Private variables
+var _view_range_cells_sq : int = view_range_cells * view_range_cells
 var _last_check_pos : Vector2 = Vector2.ZERO
-
-# Vector2 : bool
-var _checked : Array = []
-var _queue = [] # Array<Vector2>
-
 var _furthest_cell : Vector2 = Vector2.ZERO
+var _checked : Array = [] # Array<bool>
+var _queue : Array = [] # Array<Vector2>
 
 
 #-------------------------------------------------------------------------------
@@ -28,7 +29,13 @@ func get_next_best() -> Vector2:
 
 
 #-------------------------------------------------------------------------------
-func _ready():
+func set_view_range_cells(value : int) -> void:
+	view_range_cells = value
+	_view_range_cells_sq = value * value
+
+
+#-------------------------------------------------------------------------------
+func _ready() -> void:
 	var terrain_container = get_tree().get_nodes_in_group("terrain")
 	if not terrain_container.empty():
 		_terrain = terrain_container.front()
@@ -40,7 +47,7 @@ func _ready():
 
 
 #-------------------------------------------------------------------------------
-func _physics_process(_delta):
+func _physics_process(_delta) -> void:
 	if (_last_check_pos - global_position).length_squared() > RECALCULATE_DISTANCE_SQUARED:
 		_last_check_pos = global_position
 		_check_surroundings()
@@ -49,11 +56,11 @@ func _physics_process(_delta):
 
 
 #-------------------------------------------------------------------------------
-func _check_surroundings():
-	var previous_furthest = _furthest_cell
+func _check_surroundings() -> void:
 	
-	var my_cell_pos = _terrain.world_to_map(global_position)
 	var next_queue = []
+	var previous_furthest = _furthest_cell
+	var my_cell_pos = _terrain.world_to_map(global_position)
 	
 	# Assesses the furthest cell is still within range and returns the distance.
 	var furthest_dist_sq = _assess_furthest_cell(my_cell_pos)
@@ -62,11 +69,12 @@ func _check_surroundings():
 	while not _queue.empty():
 		var next_cell : Vector2 = _queue.pop_front()
 		
+		# If the cell is within range, checks all its neighbours.
 		if _is_within_range(next_cell, my_cell_pos):
-			furthest_dist_sq = _check_neighbours(next_cell, my_cell_pos, 
-													  furthest_dist_sq)
-		else:
-			next_queue.append(next_cell)
+			furthest_dist_sq = _check_neighbours(next_cell, my_cell_pos, furthest_dist_sq)
+			
+		# If the cell is outside the range, adds it to the next queue.
+		else: next_queue.append(next_cell)
 		
 	_queue = next_queue
 	
@@ -85,8 +93,8 @@ func _get_neighbours(cell : Vector2) -> Array:
 
 
 #-------------------------------------------------------------------------------
-func _is_within_range(cell_pos, my_cell_pos):
-	return (cell_pos - my_cell_pos).length_squared() <= view_range_cells_squared
+func _is_within_range(cell_pos : Vector2, my_cell_pos : Vector2) -> bool:
+	return (cell_pos - my_cell_pos).length_squared() <= _view_range_cells_sq
 
 
 #-------------------------------------------------------------------------------
@@ -121,7 +129,7 @@ func _assess_furthest_cell(my_cell_pos : Vector2) -> float:
 	var dist_squared = (_furthest_cell - my_cell_pos).length_squared()
 	
 	# Checks the furthest cell is still within range.
-	if dist_squared <= view_range_cells_squared:
+	if dist_squared <= _view_range_cells_sq:
 		return dist_squared
 	else:
 		_furthest_cell = Vector2.ZERO
@@ -129,7 +137,7 @@ func _assess_furthest_cell(my_cell_pos : Vector2) -> float:
 	
 
 #-------------------------------------------------------------------------------
-func _draw():
+func _draw() -> void:
 	for cell in _queue:
 		var global = _terrain.map_to_world_centred(cell)
 		var local = global - global_position
